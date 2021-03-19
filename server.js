e902cd28f553dd743a6119cbe8d06ff5daf1afcc
Xuân Tiến Nguyễn <xuantjen410@gmail.com>
2021-03-19 15:58:36 +0700

    save

--- server.js
+++
@@ -1,8 +0,0 @@
-const exports = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/wedding-invitation'));
-app.get('*', function (req, res){
-res.sendFile(path.join(__dirname, 'dist/wedding-invitation/index.html'));
-});
-app.listen(process.env.PORT ||8080);
